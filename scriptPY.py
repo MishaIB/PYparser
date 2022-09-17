9a49fd4f5243258c48c158e3e2ceee5d6186e82e
Миша Чукарев <mishakrut11@gmail.com>
2022-09-17 13:25:09 +0400

    added the creation of a folder

--- scriptPY.py
+++ scriptPY.py
@@ -3,10 +3,14 @@ import os
 import requests
 from bs4 import BeautifulSoup
 url='https://yandex.ru/images/search?text=cat&suggest_reqid=636214838166082117398516367577478'
+subdir='cat'
 response=requests.get(url).text
 soap= BeautifulSoup(response,"html.parser")
 links=soap.find_all("img",class_="serp-item__thumb justifier__thumb")
+if not os.path.exists('dataset' + subdir):
+    os.mkdir('dataset' + subdir)
 for link in links:
     link = link.get("src")
-    print(link)
+    urls='https://'+ link
+    print(urls)
 
