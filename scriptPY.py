9c82954c528000e3f8049299756d31b404c4ed3b
Миша Чукарев <mishakrut11@gmail.com>
2022-09-17 22:00:40 +0400

    Added save to file

--- scriptPY.py
+++ scriptPY.py
@@ -2,15 +2,21 @@ import json
 import os
 import requests
 from bs4 import BeautifulSoup
-url='https://yandex.ru/images/search?text=cat&suggest_reqid=636214838166082117398516367577478'
+url='https://yandex.ru/images/search?text=cat&from=tabbar'
 subdir='cat'
+image_number=1
 response=requests.get(url).text
 soap= BeautifulSoup(response,"html.parser")
 links=soap.find_all("img",class_="serp-item__thumb justifier__thumb")
-if not os.path.exists('dataset' + subdir):
-    os.mkdir('dataset' + subdir)
+if not os.path.exists('dataset'):
+    os.mkdir('dataset')
+if not os.path.exists(subdir):
+    os.mkdir(subdir)
 for link in links:
     link = link.get("src")
-    urls='https://'+ link
-    print(urls)
-
+    urls='https:'+ link
+    img_data=requests.get(urls,verify=False).content
+    with open(f'cat/{image_number}.jpg','wb') as file:
+        file.write(img_data)
+    image_number+=1
+    print(f'изображение {image_number}.jpg успешно скачено')
