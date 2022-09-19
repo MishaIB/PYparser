bbe42513efbf52c7853786d111bf69001fc7b350
Миша Чукарев <mishakrut11@gmail.com>
2022-09-19 15:47:43 +0400

    fixed bugs

--- scriptPY.py
+++ scriptPY.py
@@ -3,6 +3,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import time
+
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
@@ -13,39 +14,33 @@ def save_img(img_data,subdir,image_number):
 def create_file(subdir):
     if not os.path.exists(f'dataset/{subdir}'):
         os.makedirs(f'dataset/{subdir}')
-
-def search_images():
-    subdir='cat'
-    page_number=0
-    url=f'https://yandex.ru/images/search?p={page_number}&text=cat&uinfo=sw-1536-sh-864-ww-760-wh-754-pd-1.25-wp-16x9_1920x1080&lr=51&rpt=image'
-    detour=0
+def search_images(subdir):
+    create_file(subdir)
+    page_number=1
+    image_number=1
     while True:
-        create_file(subdir)
-        image_number=1
-        while image_number<50:
-            time.sleep(2)
-            response=requests.get(url,headers=headers).text
-            soap= BeautifulSoup(response,"html.parser")
-            links=soap.find_all("img",class_="serp-item__thumb justifier__thumb")
-            for link in links:
-                link = link.get("src")
-                urls='https:'+ link
-                img_data=requests.get(urls,verify=False).content
-                save_img(img_data,subdir,image_number)
-                image_number+=1
-                print(f'изображение {image_number}.jpg успешно скачено')
-            page_number+=1
-        detour+=1    
-        if detour>2:
-            print('КОНЕЦ')
-            break
-        else:
-            url=f'https://yandex.ru/images/search?p={page_number}&text=dog&lr=51&rpt=image'
-            page_number=0
-            subdir='dog'
-
+        url=f'https://yandex.ru/images/search?p={page_number}&text={subdir}&uinfo=sw-1536-sh-864-ww-760-wh-754-pd-1.25-wp-16x9_1920x1080&lr=51&rpt=image'
+        time.sleep(2)
+        response=requests.get(url,headers=headers).text
+        soap= BeautifulSoup(response,"html.parser")
+        links=soap.find_all("img",class_="serp-item__thumb justifier__thumb")
+        for link in links:
+            link = link.get("src")
+            urls='https:'+ link
+            img_data=requests.get(urls,verify=False).content
+            save_img(img_data,subdir,image_number)
+            image_number+=1
+            print(f'изображение {image_number}.jpg успешно скачено')
+        page_number+=1 
+        if image_number>50:
+            break    
 def main():
-    search_images()
+    print('Hello')
+    search_images('cat')
+    search_images('dog')
+    print('buе')
     
+if __name__ == '__main__':
+    main()
 
 
